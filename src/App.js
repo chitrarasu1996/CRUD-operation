31be2fabdd4b1d04be5aa3fc92c34c3c1fe7c81d
A.chitrarasu <chitrarasumit@gmail.com>
2023-05-23 20:35:07 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes,Route} from "react-router-dom";
-import Create from './components/Create'
+import Create from './components/create'
 import Read from './components/Read';
 
 import Update from './components/Update';
